f277f7ce6ab68f2eaad52013710ec4a517c9df8d
athiracev <athiracev@gmail.com>
2024-06-07 12:05:08 +0530

    admin profile update

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  profilePicture:String="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/1024px-User_icon_2.svg.png"
+
+  profileStatus:any=false
+  onStatus(){
+    this.profileStatus=!this.profileStatus
+  }
+
+
+  getFile(event:any){
+    const file=event.target.files[0]
+    let fr=new FileReader()
+    fr.onload=(event:any)=>{
+      console.log(event.target.result)
+      this.profilePicture=event.target.result
+      this.adminDetails.profile=event.target.result
+    }
+  }
+
+  handleSubmit(){
+    console.log(this.adminDetails)
+  }
 }
---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  server_url="http://localhost:3000"
+  
+
+  constructor() { }
+getAdmin(){
+  
+}
+
+}
